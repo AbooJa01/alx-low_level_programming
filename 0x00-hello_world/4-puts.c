73e027f6f947c0a0abad9bba2c0fccbc9bef5acd
AbooJa01 <akeemsalaam@gmail.com>
2022-09-09 02:49:54 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return:0
+ * Freestyling comments by AbooJa
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
