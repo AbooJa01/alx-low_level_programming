bd0445fd5c423fd9f2a54569bcd4cc35510812f2
AbooJa01 <akeemsalaam@gmail.com>
2022-09-10 02:13:32 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Owner: AbooJa
+ * Return: 0
+ */
+
+int main(void)
+{
+	int A;
+
+	for (A = 0; A < 10; A++)
+	{
+		printf("%d", A);
+	}
+	printf("\n");
+	return (0);
+}
