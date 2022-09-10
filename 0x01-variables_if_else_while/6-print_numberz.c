91b5c5518688a69078f2387d5730835bdb12c855
AbooJa01 <akeemsalaam@gmail.com>
2022-09-10 02:16:16 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Owner: AbooJa
+ * Return: 0
+ */
+
+int main(void)
+{
+	int A;
+
+	for (A = 48; A < 58; A++)
+		putchar(A);
+	putchar('\n');
+	return (0);
+}
