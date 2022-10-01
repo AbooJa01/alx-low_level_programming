7160825dd7d7a1f85809b783c367246d0355dfa9
AbooJa01 <akeemsalaam@gmail.com>
2022-10-01 04:27:00 +0000

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _isdigit - checks for digits
+* @c: parameter
+* AbooJa01
+* Return: Null
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
